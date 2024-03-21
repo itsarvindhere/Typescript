a28fc1779af390375f7db0adcb235446473654d3
Arvind <ranaarvind156@gmail.com>
2024-03-21 16:30:29 +0530

    tsconfig file and strictness

--- 001. The Basics/basics.js
+++
@@ -1,9 +0,0 @@
-// This is an industrial-grade general-purpose greeter function:
-// function greet(person, date) {
-//     console.log(`Hello ${person}, today is ${date}!`);
-// }
-// greet("Brendan");
-function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-}
-greet("Maddison", new Date());
--- 001. The Basics/basics.ts
+++ 001. The Basics/basics.ts
@@ -5,8 +5,19 @@
 
 // greet("Brendan");
 
-function greet(person: string, date: Date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-  }
+// function greet(person: string, date: Date) {
+//     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+//   }
    
-  greet("Maddison", new Date());
+//   greet("Maddison", new Date());
+
+// function greet(name) {
+//   console.log("Hello "+ name);
+// }
+
+// greet("John")
+// greet(10)
+// greet(true)
+
+// let userName: string = undefined;
+// console.log(userName)
